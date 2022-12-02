ac7e09d89ef1c2e6a7ad5a5d5e14b76deb53d29f
OmarPC <omaralthani5@gmail.com>
2022-12-02 19:24:06 +0400

    fixed issues in count

--- script4/attacker_report.py
+++ script4/attacker_report.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+
 
 
 import os
@@ -10,34 +10,65 @@ from geoip import geolite2
 os.system('clear')
 
 # current time
-now = time.strftime("%c") # %c is the format for the current time
+now = time.strftime("%c")
 
-print("Attacker Report - ", now) 
-failed=defaultdict(int) # create a dictionary to store the failed login attempts
-with open('syslog.log', 'r') as f: # open the syslog file
-    data=f.readlines() # read the file
+print("Attacker Report - ", now)
+failed=defaultdict(int)
+with open('syslog.log', 'r') as f:
+    data=f.readlines()
     # print(data)
-    for line in data: # loop through the file
-        line=line.strip() # remove the newline character
-        if 'Failed password for root from'  in line: 
-            # if the line contains the string 'Failed password for root from
-            # #store the IP address if it is not already in the dictionary else increment the count
-            ip=line.split(' ')[10] # split the line and get the IP address
-            # print(ip)
-            failed[ip]+=1 # increment the count
-        elif 'Failed password for invalid user' in line: # if the line contains the string 'Failed password for invalid user'
-            # print(line)
-            ip=line.split('from')[1].split('port')[0].strip() # split the line and get the IP address
+    ip=''
+    for line in data:
+        line=line.strip()
+        if 'Failed password for root from'  in line:
+            #store the IP address if it is not already in the dictionary else increment the count
+            ip=line.split(' ')[10]
             # print(ip)
             failed[ip]+=1
-# keep only attackers with more than 10 failed attempts
-failed={k:v for k,v in failed.items() if v>=10} 
-failed=sorted(failed.items(), key=lambda x: x[1]) # sort the dictionary by the value
+        elif 'Vagrantfile from' in line:
+            # print(line)
+            ip=line.split('from')[1].strip()
+            if 'port' in ip:
+                ip=ip.split('port')[0].strip()
+            failed[ip]+=1
+
+        elif 'Failed password for invalid user' in line:
+            # print(line)
+            ip=line.split('from')[1].split('port')[0].strip()
+            failed[ip]+=1
+        elif 'rhost' in line:
+            ip=line.split('rhost=')[1].split(' ')[0]
+            failed[ip]+=1
+        elif 'Received disconnect from' in line:
+            ip=line.split(' ')[8].split(':')[0]
+            failed[ip]+=1
+        elif 'Connection closed by' in line:
+            ip=line.split(' ')[8]
+            failed[ip]+=1
+
+        elif 'Invalid user' in line:
+            ip=line.split(' ')[9]
+            failed[ip]+=1
+        elif 'Did not receive identification string from' in line:
+            ip=line.split(' ')[11]
+            failed[ip]+=1
+        elif 'Failed password for' in line:
+            ip=line.split('from')[1].split('port')[0].strip()
+            failed[ip]+=1
+
+# remove the IP address from the dictionary if the count is less than 10
+for ip in list(failed):
+    if failed[ip] < 10:
+        del failed[ip]
+# print(failed)
+
+            
+failed=sorted(failed.items(), key=lambda x: x[1])
 print("COUNT\t\t\tIP ADDRESS\t\tCOUNTRY")
-for i in failed: 
-    country=geolite2.lookup(i[0]) # get the country of the IP address
-    if country is not None: 
-        if len(i[0]) < 8: # if the IP address is less than 8 characters
-            print(str(i[1]) + "\t\t\t" + i[0] + "\t\t\t" + country.country) # print the count, IP address and country
-        elif len(i[0]) < 16: # if the IP address is less than 16 characters
-            print(str(i[1]) + "\t\t\t" + i[0] + "\t\t" + country.country) # print the count, IP address and country
+for i in failed:
+    country=geolite2.lookup(i[0])
+    if country is not None:
+        if len(i[0]) < 8:
+            print(str(i[1]) + "\t\t\t" + i[0] + "\t\t\t" + country.country)
+        elif len(i[0]) < 16:
+            print(str(i[1]) + "\t\t\t" + i[0] + "\t\t" + country.country)
